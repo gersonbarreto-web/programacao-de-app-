e71389274672c7e1c3b1c5f78fa2213eca7fa5ed
gerson <kayo.sampaio@ba.estudante.senai.br>
2026-03-26 16:54:12 -0300

    projeto

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import './>style.css'
+
+function Mensagem() {
+    return (
+        // comando html
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Estes e um componente com uma mensagem simples.</p>
+        </div>
+    )
+}
+
+export default Mensagem
